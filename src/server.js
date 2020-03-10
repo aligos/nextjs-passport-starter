fb68de20de3169e2043bef0e6c93c7acf1e2d1d5
Rahmat Aligos <rahmat@coconutlab.io>
2020-03-10 09:13:57 +0000

    feat: add auth0 server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const https = require('https');
 const { readFileSync } = require('fs');
 const express = require('express');
 const next = require('next');
+const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const uid = require('uid-safe');
+
+const authRoutes = require('./auth-routes');
 
 const dev = process.env.NODE_ENV !== 'production';
 
@@ -21,10 +27,50 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+
+  // Add session management to Express
+  const sessionConfig = {
+    secret: uid.sync(18),
+    cookie: {
+      maxAge: 86400 * 1000, // 24 hours in milliseconds
+      secure: server.get('env') === 'production',
+    },
+    resave: false,
+    saveUninitialized: true,
+  };
+
+  server.use(session(sessionConfig));
+
+  // Configuring Auth0Strategy
+  const auth0Strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL: process.env.AUTH0_CALLBACK_URL,
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      return done(null, profile);
+    }
+  );
+
+  // Configuring Passport
+  passport.use(auth0Strategy);
+  passport.serializeUser((user, done) => done(null, user));
+  passport.deserializeUser((user, done) => done(null, user));
+
+  // Adding Passport and authentication routes
+  server.use(passport.initialize());
+  server.use(passport.session());
+
   // Define app routes
+  server.use(authRoutes);
 
   // handling everything else with Next.js
-  server.get('*', handle);
+  server.get('*', (req, res, next) => {
+    if (!req.isAuthenticated()) return res.redirect('/login');
+    handle(req, res, next);
+  });
 
   // we can add some utils from /bin/www
   http.createServer(server).listen(process.env.PORT, () => {
